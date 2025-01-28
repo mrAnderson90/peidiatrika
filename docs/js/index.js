84967f02ccb116452ec76bfc1090e73589071c03
Bravo <bravo@Mac-mini-Bravo.local>
2025-01-28 23:16:50 +0300

    Add swiper slider

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,1 +1,14 @@
-console.log('test index.js');
+// Swiper
+const swiperParams = {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.slider__control--next',
+    prevEl: '.slider__control--previous',
+  },
+  pagination: {
+    el: '.news__dots',
+  }
+};
+
+const swiper = new Swiper('.swiper', swiperParams);
